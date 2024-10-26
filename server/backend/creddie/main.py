b7cf401ba91434ee53b421f0047642677ada0351
Jakal02 <gitcontribs@eligrubbs.com>
2024-10-26 13:13:18 -0500

    Added API version variable.

--- server/backend/creddie/main.py
+++ server/backend/creddie/main.py
@@ -3,8 +3,14 @@
 """
 from fastapi import FastAPI
 
+# The version must be changed manually here.
+API_VERSION = "0.1.0"
 
-app = FastAPI()
+app = FastAPI(
+    version = API_VERSION,
+    title = "Creddie",
+    description="A Spending Tracker For The Future!"
+)
 
 
 @app.get("/")
