7f714b6f5dcd1b4acbfa16c8ceb171ef0fad718b
Jakal02 <gitcontribs@eligrubbs.com>
2024-10-27 14:43:48 -0500

    datetime function for default date entries in tables.

--- server/backend/creddie/utils/tools.py
+++ server/backend/creddie/utils/tools.py
@@ -1,12 +1,14 @@
 from typing import Optional
+
 import string
 import random
 from random import Random
+from datetime import datetime, timezone
 
 CHARS_FOR_UUID = string.ascii_uppercase + string.digits
 
 
-def get_8_digit_UUID(rdm_gen: Optional[Random] = None):
+def get_8_digit_UUID(rdm_gen: Optional[Random] = None) -> str:
     """Return an 8 digit UUID comprised of only capital letters and digits.
 
     `rdm_gen`: An optional `random.Random` object to be used instead of a seedless call to a `random` function. 
@@ -15,3 +17,12 @@ def get_8_digit_UUID(rdm_gen: Optional[Random] = None):
     if isinstance(rdm_gen, Random):
         return ''.join([rdm_gen.choice(CHARS_FOR_UUID) for _ in range(8)])
     return ''.join([random.choice(CHARS_FOR_UUID) for _ in range(8)])
+
+
+def utc_now() -> datetime:
+    """Callable wrapper for datetime utc now function.
+
+    Return a datetime representing the current time in the UTC timezone.
+    """
+    tz_aware = datetime.now(timezone.utc)
+    return tz_aware
