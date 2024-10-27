7883baf7480d2ea2734677b45d117590e78a3d70
Jakal02 <gitcontribs@eligrubbs.com>
2024-10-27 15:32:46 -0500

    removed utf8 collation.

--- server/backend/creddie/models/transaction_model.py
+++ server/backend/creddie/models/transaction_model.py
@@ -1,31 +1,32 @@
 """
 Transaction Data Model.
 """
-from sqlalchemy import Column, String, Numeric, Boolean, TIMESTAMP
+from sqlalchemy import Column, String, Numeric, Boolean, TIMESTAMP, ForeignKey
 
 from ..utils.tools import get_8_digit_UUID, utc_now
 from . import Base
+from .category_model import TxnCategory
 
 class Transaction(Base):
     __tablename__ = "transactions"
 
     # Enriched
-    id = Column(String(8, collation="utf8"), default=get_8_digit_UUID, nullable=False, primary_key=True, unique=True)
+    id = Column(String(8), default=get_8_digit_UUID, nullable=False, primary_key=True, unique=True)
     updated_date = Column(TIMESTAMP(timezone=True), default=utc_now, onupdate=utc_now, nullable=False)
     entry_date = Column(TIMESTAMP(timezone=True), default=utc_now, nullable=False)
 
     # Required
     amount = Column(Numeric(scale=2), nullable=False)
-    currency = Column(String(4, collation="utf8"), nullable=False)
+    currency = Column(String(4), nullable=False)
     is_credit = Column(Boolean, nullable=False)
     transaction_date = Column(TIMESTAMP(timezone=True), nullable=False)
 
-    other_party = Column(String(64, collation="utf8"), nullable=False)
-    transaction_description = Column(String(256, collation="utf8"), nullable=False)
-    category = Column(String(64, collation="utf8"), nullable=False)
+    other_party = Column(String(64), nullable=False)
+    transaction_description = Column(String(256), nullable=False)
+    category_id = Column(String(8), ForeignKey(TxnCategory.id), nullable=False)
 
     # Optional
     is_gift = Column(Boolean, default=False, nullable=False)
-    gift_party = Column(String(64, collation="utf8"), nullable=False, default="")
+    gift_party = Column(String(64), nullable=False, default="")
     is_marketplace = Column(Boolean, default=False, nullable=False)
-    marketplace_party = Column(String(64, collation="utf8"), nullable=False, default="")
+    marketplace_party = Column(String(64), nullable=False, default="")
