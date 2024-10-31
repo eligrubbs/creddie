bbad1e69bc5abfddc09167e8693e144a3d61bfa0
Jakal02 <gitcontribs@eligrubbs.com>
2024-10-30 22:12:55 -0500

    stubbed category table crud.

---
+++ server/backend/creddie/crud/category_crud.py
@@ -0,0 +1,19 @@
+
+
+class CategoryCRUD():
+
+    def create():
+        """Create Category entry in table."""
+
+    def delete():
+        """Delete Category entry in table."""
+
+    def update():
+        """Update an entry in Category table.
+        """
+
+    def read():
+        """Read an entry in Category table."""
+
+
+categories = CategoryCRUD()
