02a08e65e34b3d81f8f678c6a2151cf2788374ef
Jakal02 <gitcontribs@eligrubbs.com>
2024-10-30 22:14:30 -0500

    updated utils to use consts.

--- server/backend/creddie/utils/tools.py
+++ server/backend/creddie/utils/tools.py
@@ -1,22 +1,21 @@
 from typing import Optional
 
-import string
 import random
 from random import Random
 from datetime import datetime, timezone
 
-CHARS_FOR_UUID = string.ascii_uppercase + string.digits
+from ..consts import CHARS_FOR_UUID, UUID_MAX_LEN
 
 
-def get_8_digit_UUID(rdm_gen: Optional[Random] = None) -> str:
-    """Return an 8 digit UUID comprised of only capital letters and digits.
+def get_UUID(rdm_gen: Optional[Random] = None) -> str:
+    """Return a `UUID_MAX_LEN` digit UUID comprised of only capital letters and digits.
 
     `rdm_gen`: An optional `random.Random` object to be used instead of a seedless call to a `random` function. 
     If something other than a `random.Random` object is passed, this parameter is ignored.
     """
     if isinstance(rdm_gen, Random):
-        return ''.join([rdm_gen.choice(CHARS_FOR_UUID) for _ in range(8)])
-    return ''.join([random.choice(CHARS_FOR_UUID) for _ in range(8)])
+        return ''.join([rdm_gen.choice(CHARS_FOR_UUID) for _ in range(UUID_MAX_LEN)])
+    return ''.join([random.choice(CHARS_FOR_UUID) for _ in range(UUID_MAX_LEN)])
 
 
 def utc_now() -> datetime:
