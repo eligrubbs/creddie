1b08e0fc7c4cf874109106f1fe44e2329e213e11
Jakal02 <gitcontribs@eligrubbs.com>
2024-10-30 22:15:02 -0500

    substituted consts in tables.

--- server/backend/creddie/models/category_model.py
+++ server/backend/creddie/models/category_model.py
@@ -3,16 +3,17 @@ Transaction Category Model.
 """
 from sqlalchemy import Column, String, TIMESTAMP
 
-from ..utils.tools import get_8_digit_UUID, utc_now
+from ..utils.tools import get_UUID, utc_now
 from . import Base
+from ..consts import UUID_MAX_LEN, CATEGORY_MAX_NAME_LEN
 
 class TxnCategory(Base):
     __tablename__ = "txncategories"
 
     # Enriched
-    id = Column(String(8), default=get_8_digit_UUID, nullable=False, primary_key=True, unique=True)
+    id = Column(String(UUID_MAX_LEN), default=get_UUID, nullable=False, primary_key=True, unique=True)
     updated_date = Column(TIMESTAMP(timezone=True), default=utc_now, onupdate=utc_now, nullable=False)
     created_date = Column(TIMESTAMP(timezone=True), default=utc_now, nullable=False)
 
     # Required
-    name = Column(String(32), nullable=False)
+    name = Column(String(CATEGORY_MAX_NAME_LEN), nullable=False, unique=True)
--- server/backend/creddie/models/transaction_model.py
+++ server/backend/creddie/models/transaction_model.py
@@ -3,30 +3,31 @@ Transaction Data Model.
 """
 from sqlalchemy import Column, String, Numeric, Boolean, TIMESTAMP, ForeignKey
 
-from ..utils.tools import get_8_digit_UUID, utc_now
+from ..utils.tools import get_UUID, utc_now
 from . import Base
 from .category_model import TxnCategory
+from ..consts import UUID_MAX_LEN, TBL_MAX_PARTY_LEN, TBL_MAX_CURRENCY_LEN, TBL_AMOUNT_DECIMAL_POINTS, TBL_MAX_DESC_LEN
 
 class Transaction(Base):
     __tablename__ = "transactions"
 
     # Enriched
-    id = Column(String(8), default=get_8_digit_UUID, nullable=False, primary_key=True, unique=True)
+    id = Column(String(UUID_MAX_LEN), default=get_UUID, nullable=False, primary_key=True, unique=True)
     updated_date = Column(TIMESTAMP(timezone=True), default=utc_now, onupdate=utc_now, nullable=False)
     entry_date = Column(TIMESTAMP(timezone=True), default=utc_now, nullable=False)
 
     # Required
-    amount = Column(Numeric(scale=2), nullable=False)
-    currency = Column(String(4), nullable=False)
+    amount = Column(Numeric(scale=TBL_AMOUNT_DECIMAL_POINTS), nullable=False)
+    currency = Column(String(TBL_MAX_CURRENCY_LEN), nullable=False)
     is_credit = Column(Boolean, nullable=False)
     transaction_date = Column(TIMESTAMP(timezone=True), nullable=False)
 
-    other_party = Column(String(64), nullable=False)
-    transaction_description = Column(String(256), nullable=False)
-    category_id = Column(String(8), ForeignKey(TxnCategory.id), nullable=False)
+    other_party = Column(String(TBL_MAX_PARTY_LEN), nullable=False)
+    transaction_description = Column(String(TBL_MAX_DESC_LEN), nullable=False)
+    category_id = Column(String(UUID_MAX_LEN), ForeignKey(TxnCategory.id), nullable=False)
 
     # Optional
     is_gift = Column(Boolean, default=False, nullable=False)
-    gift_party = Column(String(64), nullable=False, default="")
+    gift_party = Column(String(TBL_MAX_PARTY_LEN), nullable=False, default="")
     is_marketplace = Column(Boolean, default=False, nullable=False)
-    marketplace_party = Column(String(64), nullable=False, default="")
+    marketplace_party = Column(String(TBL_MAX_PARTY_LEN), nullable=False, default="")
