edd1d30b9682156b3d963be96367103ff9ddc51b
Jakal02 <gitcontribs@eligrubbs.com>
2024-11-02 15:18:31 -0500

    test create category. added utils for other tests to use too.

---
+++ server/backend/tests/crud/test_category_crud.py
@@ -0,0 +1,20 @@
+import pytest
+from sqlalchemy.orm import Session
+
+from creddie.schemas.category_schema import CreateCategory
+from creddie.models.category_model import TxnCategory
+from creddie.crud.category_crud import categories
+
+from ..utils import num_rows_in_tbl
+from ..utils.models import random_category_name
+
+def test_create_category(sess: Session):
+    rows = num_rows_in_tbl(sess, TxnCategory)
+
+    create_data = CreateCategory(name=random_category_name())
+    categories.create(sess, obj_in = create_data)
+
+    rows_after = num_rows_in_tbl(sess, TxnCategory)
+
+    assert rows+1 == rows_after
+
---
+++ server/backend/tests/utils/models.py
@@ -0,0 +1,4 @@
+from creddie.utils.tools import get_UUID
+
+def random_category_name():
+    return f"Category {get_UUID()}"
