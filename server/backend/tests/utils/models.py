304e14300353f76e1a747d99bacb3a53cbe8d3af
Jakal02 <gitcontribs@eligrubbs.com>
2024-11-02 16:20:21 -0500

    finished CRUD tests for categories.

--- server/backend/tests/utils/models.py
+++ server/backend/tests/utils/models.py
@@ -1,4 +1,14 @@
+from sqlalchemy.orm import Session
+
+from creddie.models import TxnCategory
 from creddie.utils.tools import get_UUID
 
 def random_category_name():
     return f"Category {get_UUID()}"
+
+def create_random_category(sess: Session):
+    cat = TxnCategory(name=random_category_name())
+    sess.add(cat)
+    sess.commit()
+    sess.refresh(cat)
+    return cat
