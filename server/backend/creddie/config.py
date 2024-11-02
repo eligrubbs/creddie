dac41adee8d9666b6e5b5d2842c36ba05e174f6c
Jakal02 <gitcontribs@eligrubbs.com>
2024-11-02 15:15:35 -0500

    Added testing option.

--- server/backend/creddie/config.py
+++ server/backend/creddie/config.py
@@ -1,4 +1,13 @@
+from enum import Enum
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class ModeEnum(str, Enum):
+    """Enumeration for possible api environment states."""
+
+    development = "development"
+    production = "production"
+    testing = "testing"
 
 
 class APISettings(BaseSettings):
@@ -7,12 +16,14 @@ class APISettings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="allow"
     )
-
+    Mode: ModeEnum = ModeEnum.development
     DATABASE_URI: str
     ECHO_DATABASE: bool = True
 
     def get_db_uri_string(self) -> str:
         """Return the DB URI."""
+        if self.Mode == ModeEnum.testing:
+            return "sqlite:///./TestDB.db"
         return self.DATABASE_URI
 
 
