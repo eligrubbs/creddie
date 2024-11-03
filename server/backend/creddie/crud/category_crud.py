e493f6e1d8e4650a37f138b3b96bcbe34869e990
Jakal02 <gitcontribs@eligrubbs.com>
2024-11-03 14:24:29 -0600

    access categories by name.

--- server/backend/creddie/crud/category_crud.py
+++ server/backend/creddie/crud/category_crud.py
@@ -1,6 +1,9 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
 from ..models import TxnCategory
 from .base import CRUDBase
-from ..schemas.types import UUIDType
+from ..schemas.types import UUIDType, CatNameType
 from ..schemas.category_schema import CreateCategory, UpdateCategory
 
 
@@ -13,5 +16,13 @@ class CategoryCRUD(CRUDBase[TxnCategory, UUIDType, CreateCategory, UpdateCategor
         """Initialize the class."""
         super().__init__(*args, **kwargs)
 
+
+    def get_by_name(self, sess: Session, *, name: CatNameType | str):
+        if isinstance(name, str):
+            name = CatNameType(name)
+        get_stmt = select(self.model).where(getattr(self.model, "name") == name.get())
+        result = sess.execute(get_stmt)
+        return result.scalar_one_or_none()
+        
 
 categories = CategoryCRUD(TxnCategory)
