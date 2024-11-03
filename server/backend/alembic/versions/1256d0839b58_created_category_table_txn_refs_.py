21a3ba3ea471113710767bf26bbb70a56b7dce11
Jakal02 <gitcontribs@eligrubbs.com>
2024-11-03 13:41:18 -0600

    removed Hero table.

--- server/backend/alembic/versions/1256d0839b58_created_category_table_txn_refs_.py
+++ server/backend/alembic/versions/1256d0839b58_created_category_table_txn_refs_.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '1256d0839b58'
-down_revision: Union[str, None] = 'ca0b5484b1ea'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
