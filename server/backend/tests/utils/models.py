ddc268266079f5bd66be4a414be0a00558c2298e
Jakal02 <gitcontribs@eligrubbs.com>
2024-11-03 13:35:38 -0600

    utils for transaction tests.

--- server/backend/tests/utils/models.py
+++ server/backend/tests/utils/models.py
@@ -1,8 +1,12 @@
 from sqlalchemy.orm import Session
+from pydantic import AwareDatetime
 
-from creddie.models import TxnCategory
-from creddie.utils.tools import get_UUID
+from creddie.schemas.types import PartyType, UUIDType
+from creddie.schemas.transaction_schema import CreateTransaction
+from creddie.models import TxnCategory, Transaction
+from creddie.utils.tools import get_UUID, utc_now
 
+# Category
 def random_category_name():
     return f"Category {get_UUID()}"
 
@@ -12,3 +16,27 @@ def create_random_category(sess: Session):
     sess.commit()
     sess.refresh(cat)
     return cat
+
+
+# Transaction
+def random_transaction_info() -> CreateTransaction:
+    return CreateTransaction(**{
+        "amount": 200.20,
+        "currency": "USD",
+        "is_credit": False,
+        "transaction_date": utc_now(),
+        "other_party": PartyType(f"Other Party {get_UUID()}").get(),
+        "transaction_description": f"{get_UUID()*3}",
+        "category_id": UUIDType(get_UUID()).get(),
+        "is_gift": False,
+        "gift_party": PartyType(f"Gift Party {get_UUID()}").get(),
+        "is_marketplace": False,
+        "marketplace_party": PartyType(f"Marketplace {get_UUID()}").get(),
+    })
+
+def create_random_transaction(sess: Session):
+    txn = Transaction(**random_transaction_info().model_dump())
+    sess.add(txn)
+    sess.commit()
+    sess.refresh(txn)
+    return txn
