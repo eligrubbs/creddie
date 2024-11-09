9163e388d3dbf0b38b96364a8761f7bf69def7b6
Jakal02 <gitcontribs@eligrubbs.com>
2024-11-09 10:24:22 -0600

    remove tz arg, it isnt used anywhere.

--- server/backend/creddie/models/transaction_model.py
+++ server/backend/creddie/models/transaction_model.py
@@ -14,13 +14,13 @@ class Transaction(Base):
     # Enriched
     id = Column(String(UUID_MAX_LEN), default=get_UUID, nullable=False, primary_key=True, unique=True)
     updated_at = Column(DateTime, default=utc_now_naive, onupdate=utc_now_naive, nullable=False)
-    created_at = Column(DateTime(timezone=True), default=utc_now_naive, nullable=False)
+    created_at = Column(DateTime, default=utc_now_naive, nullable=False)
 
     # Required
     amount = Column(Numeric(scale=TBL_AMOUNT_DECIMAL_POINTS), nullable=False)
     currency = Column(String(TBL_MAX_CURRENCY_LEN), nullable=False)
     is_credit = Column(Boolean, nullable=False)
-    transaction_date = Column(DateTime(timezone=True), nullable=False)
+    transaction_date = Column(DateTime, nullable=False)
 
     other_party = Column(String(TBL_MAX_PARTY_LEN), nullable=False)
     transaction_description = Column(String(TBL_MAX_DESC_LEN), nullable=False)
