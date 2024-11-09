18188034c443139fff3a685855599519ddef6f74
Jakal02 <gitcontribs@eligrubbs.com>
2024-11-09 17:11:16 -0600

    Defaults for API docs added. Good for testing.

--- server/backend/creddie/schemas/transaction_schema.py
+++ server/backend/creddie/schemas/transaction_schema.py
@@ -1,20 +1,40 @@
 from typing import Optional, Annotated
 from decimal import Decimal
+from datetime import datetime
 
 from pydantic import BaseModel, NaiveDatetime, ConfigDict, Field
 
-from .types import UUIDType, PartyType, CurrencyType
+from .types import UUIDType, PartyType, CurrencyType, TZType
 from .metaclass import partial_model
 from ..consts import TBL_MAX_DESC_LEN
 
 
 class CreateTransaction(BaseModel):
     """Info required to create a transaction."""
-    amount: Annotated[Decimal, Field(decimal_places=2)]
+    model_config: ConfigDict = {
+        "json_schema_extra": {
+            "examples": [{
+                "amount": "20.0",
+                "currency": "USD",
+                "is_credit": True,
+                "transaction_date": datetime.now(),
+                "transaction_tz": "America/New_York",
+                "other_party": "Walmart",
+                "transaction_description": "a transaction.",
+                "category_id": "REQUIRED",
+                "is_gift": False,
+                "gift_party": "",
+                "is_marketplace": False,
+                "marketplace_party": ""
+            }]
+        }
+    }
+
+    amount: Annotated[Decimal, Field(decimal_places=2, gt=0)]
     currency: CurrencyType
     is_credit: bool
     transaction_date: NaiveDatetime
-    transaction_tz: str
+    transaction_tz: TZType
 
     other_party: PartyType
     transaction_description: Annotated[str, Field(max_length=TBL_MAX_DESC_LEN)]
@@ -33,7 +53,7 @@ class ReadTransaction(BaseModel):
     updated_at: NaiveDatetime
     created_at: NaiveDatetime
 
-    amount: Annotated[Decimal, Field(decimal_places=2)]
+    amount: Annotated[Decimal, Field(decimal_places=2, gt=0)]
     currency: CurrencyType
     is_credit: bool
     transaction_date: NaiveDatetime
@@ -54,9 +74,27 @@ class ReadTransaction(BaseModel):
 class UpdateTransaction(BaseModel):
     """Info in a transaction able to be updated."""
 
-    model_config: ConfigDict = {"extra": "forbid"}
+    model_config: ConfigDict = {
+        "extra": "forbid",
+        "json_schema_extra": {
+            "examples": [{
+                "amount": "20.0",
+                "currency": "USD",
+                "is_credit": True,
+                "transaction_date": datetime.now(),
+                "transaction_tz": "America/New_York",
+                "other_party": "Walmart",
+                "transaction_description": "a transaction.",
+                "category_id": "REQUIRED",
+                "is_gift": False,
+                "gift_party": "",
+                "is_marketplace": False,
+                "marketplace_party": ""
+            }]
+        }
+    }
 
-    amount: Annotated[Decimal, Field(decimal_places=2)]
+    amount: Annotated[Decimal, Field(decimal_places=2, gt=0)]
     currency: CurrencyType
     is_credit: bool
     transaction_date: NaiveDatetime
