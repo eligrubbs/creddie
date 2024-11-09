1c75e26714011cd02d0175edccff101d1de308ec
Jakal02 <gitcontribs@eligrubbs.com>
2024-11-09 17:11:29 -0600

    Added transaction routes.

--- server/backend/creddie/api/v1/router.py
+++ server/backend/creddie/api/v1/router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 
 from .endpoints import (
-    categories
+    categories,
+    transactions
 )
 
 api_router = APIRouter()
-api_router.include_router(categories.router, tags=["category"])
+api_router.include_router(categories.router, prefix="/category", tags=["category"])
+api_router.include_router(transactions.router, prefix="/transaction", tags=["transaction"])
