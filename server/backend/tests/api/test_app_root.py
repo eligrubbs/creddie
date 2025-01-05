08bfed665b780cd863d264aedff96e930829b201
Jakal02 <gitcontribs@eligrubbs.com>
2025-01-05 16:43:03 -0600

    Removed lingering frontend-on-backend testing code.

--- server/backend/tests/api/test_app_root.py
+++ server/backend/tests/api/test_app_root.py
@@ -1,21 +1,10 @@
-import pathlib
 import pytest
-from fastapi.templating import Jinja2Templates
-from jinja2 import Template
 from httpx import AsyncClient, Response
-
-creddie_dir = pathlib.Path(__file__).parent.parent.parent.resolve() / "creddie"
-templates = Jinja2Templates(directory=creddie_dir / "templates")
 
 @pytest.mark.anyio
 async def test_app_root(client: AsyncClient):
-    """Test the / GET route."""
-    # expected_response: Template = templates.get_template(
-    #     name="index.html"
-    # ).render({"message": "Creddie says Hello!"})
+    """Test the / GET route."""\
 
     resp: Response = await client.get("/")
-
-    # assert expected_response == resp.read().decode()
 
     assert resp.json() == {"message": "Creddie says Hello!"}
